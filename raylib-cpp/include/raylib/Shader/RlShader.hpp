3f853df85bfdc1fc7015d88a3a193c7f5a9b53a0
hugo <hugo.baret@epitech.eu>
2022-06-07 13:09:54 +0400

    Shader object

--- raylib-cpp/include/raylib/Shader/RlShader.hpp
+++ raylib-cpp/include/raylib/Shader/RlShader.hpp
@@ -17,16 +17,18 @@ namespace raylib {
         public:
             // Shader management functions
             // NOTE: Shader functionality is not available on OpenGL 1.1
-            Shader loadShader(const std::string &vsFileName, const std::string &fsFileName);      // Load shader from files and bind default locations
-            Shader loadShaderFromMemory(const std::string &vsCode, const std::string &fsCode);    // Load shader from code strings and bind default locations
-            int getShaderLocation(Shader shader, const std::string &uniformName);          // Get shader uniform location
-            int getShaderLocationAttrib(Shader shader, const std::string &attribName);     // Get shader attribute location
-            void setShaderValue(Shader shader, int locIndex, const void *value, int uniformType);               // Set shader uniform value
-            void setShaderValueV(Shader shader, int locIndex, const void *value, int uniformType, int count);   // Set shader uniform value vector
-            void setShaderValueMatrix(Shader shader, int locIndex, Matrix mat);         // Set shader uniform value (matrix 4x4)
-            void setShaderValueTexture(Shader shader, int locIndex, Texture2D texture); // Set shader uniform value for texture (sampler2d)
-            void unloadShader(Shader shader);                                       // Unload shader from GPU memory (VRAM)
+            RlShader();
+            ~RlShader();
+            void load(const std::string &vsFileName, const std::string &fsFileName);      // Load shader from files and bind default locations
+            void loadFromMemory(const std::string &vsCode, const std::string &fsCode);    // Load shader from code strings and bind default locations
+            int getLocation(const std::string &uniformName);          // Get shader uniform location
+            int getLocationAttrib(const std::string &attribName);     // Get shader attribute location
+            void setValue(int locIndex, const void *value, int uniformType);               // Set shader uniform value
+            void setValueV(int locIndex, const void *value, int uniformType, int count);   // Set shader uniform value vector
+            void setValueMatrix(int locIndex, Matrix mat);         // Set shader uniform value (matrix 4x4)
+            void setValueTexture(int locIndex, Texture2D texture); // Set shader uniform value for texture (sampler2d)
         private:
+            Shader _shader;
     };
 }
 
