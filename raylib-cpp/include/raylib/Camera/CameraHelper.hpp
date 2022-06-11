caa7bb2e3fc66c0f764239aba8804703dc683c4f
Alwyn Mattapullut <alwyn.mattapullut@epitech.eu>
2022-06-11 11:31:19 +0400

    ♻️ Refactor RlCamera

---
+++ raylib-cpp/include/raylib/Camera/CameraHelper.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2022
+** CameraHelper.hpp
+** File description:
+** CameraHelper.hpp
+*/
+
+#ifndef INDIESTUDIO_CAMERAHELPER_HPP
+#define INDIESTUDIO_CAMERAHELPER_HPP
+
+#include "raylib.h"
+
+namespace raylib {
+    class CameraHelper {
+    public:
+        /**
+         * @brief Set camera pan key to combine with mouse movement (free camera)
+         * @param keyPan the key to combine with mouse movement
+         */
+        static void setPanControl(int keyPan);
+
+        /**
+         * @brief Set camera alt key to combine with mouse movement (free camera)
+         * @param keyAlt the key to combine with mouse movement
+         */
+        static void setAltControl(int keyAlt);
+
+        /**
+         * @brief Set camera smooth zoom key to combine with mouse movement (free camera)
+         * @param keySmoothZoom the key to combine with mouse movement
+         */
+        static void setSmoothZoomControl(int keySmoothZoom);
+
+        /**
+         * @brief Set camera move controls (1st person and 3rd person cameras)
+         * @param keyFront the key to forward
+         * @param keyBack the key to backward
+         * @param keyRight the key to right
+         * @param keyLeft the key to left
+         * @param keyUp the key to up
+         * @param keyDown the key to down
+         */
+        static void setMoveControls(int keyFront, int keyBack, int keyRight, int keyLeft, int keyUp, int keyDown);
+    };
+}
+
+#endif //INDIESTUDIO_CAMERAHELPER_HPP
--- raylib-cpp/src/raylib/Camera/RlCamera.cpp
+++ raylib-cpp/src/raylib/Camera/RlCamera.cpp
@@ -7,23 +7,12 @@
 
 #include "raylib/Camera/RlCamera.hpp"
 
-// Camera System Functions (Module: r_camera)
-
-raylib::RlCamera::RlCamera() : _camera()
+raylib::RlCamera::RlCamera(const Vector3f &position, const Vector3f &target, const Vector3f &up, const float &fovy, const CameraProjection &projection) : _camera()
 {
-    this->_camera.position = { 10.0f, 10.0f, 10.0f }; // Camera position
-    this->_camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    this->_camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    this->_camera.fovy = 45.0f;                                // Camera field-of-view Y
-    this->_camera.projection = CAMERA_PERSPECTIVE;
-}
-
-raylib::RlCamera::RlCamera(Vector3f position, Vector3f target, Vector3f up, float fovy, int projection) : _camera()
-{
-    this->_camera.position = raylib::VectorHelper::toRaylibVector(position); // Camera position
-    this->_camera.target = raylib::VectorHelper::toRaylibVector(target);      // Camera looking at point
-    this->_camera.up = raylib::VectorHelper::toRaylibVector(up);          // Camera up vector (rotation towards target)
-    this->_camera.fovy = fovy;                                // Camera field-of-view Y
+    this->_camera.position = raylib::VectorHelper::toRaylibVector(position);
+    this->_camera.target = raylib::VectorHelper::toRaylibVector(target);
+    this->_camera.up = raylib::VectorHelper::toRaylibVector(up);
+    this->_camera.fovy = fovy;
     this->_camera.projection = projection;
 }
 
@@ -32,40 +21,19 @@ void raylib::RlCamera::update()
     UpdateCamera(&this->_camera);
 }
 
-Camera3D raylib::RlCamera::get()
+const Camera3D &raylib::RlCamera::get() const
 {
     return this->_camera;
 }
 
-void raylib::RlCamera::setPosition(Vector3f position)
+void raylib::RlCamera::setPosition(const Vector3f &position)
 {
     this->_camera.position = raylib::VectorHelper::toRaylibVector(position);
 }
 
-void raylib::RlCamera::setTarget(Vector3f target)
+void raylib::RlCamera::setTarget(const Vector3f &target)
 {
     this->_camera.target = raylib::VectorHelper::toRaylibVector(target);
-}
-
-void raylib::RlCamera::setPanControl(int keyPan)
-{
-    SetCameraPanControl(keyPan);
-}
-
-void raylib::RlCamera::setAltControl(int keyAlt)
-{
-    SetCameraAltControl(keyAlt);
-}
-
-void raylib::RlCamera::setSmoothZoomControl(int keySmoothZoom)
-{
-    SetCameraSmoothZoomControl(keySmoothZoom);
-
-}
-
-void raylib::RlCamera::setMoveControls(int keyFront, int keyBack, int keyRight, int keyLeft, int keyUp, int keyDown)
-{
-    SetCameraMoveControls(keyFront, keyBack, keyRight, keyLeft, keyUp, keyDown);
 }
 
 Matrix raylib::RlCamera::geMatrix()
