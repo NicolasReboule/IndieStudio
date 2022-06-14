8e1ad94c490a8ee2b6c600d01d180597a5836cab
Alwyn Mattapullut <alwyn.mattapullut@epitech.eu>
2022-06-14 22:37:36 +0400

    🔀 Merged gameEngine (aka the game lol) into dev

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include "IndieStudio.hpp"
 
+/*
 int main(int ac, char **av)
 {
     auto window = raylib::window::RlWindow::getInstance();
@@ -32,5 +33,37 @@ int main(int ac, char **av)
 
     while (window->isOpen())
         sceneManager->makeLoop(camera);
+    return (0);
+}
+*/
+
+int main(int ac, char **av)
+{
+    auto window = raylib::window::RlWindow::getInstance();
+    window->createWindow("Bomberman", 1280, 720, 120);
+    raylib::RlCamera camera = raylib::RlCameraBuilder().setCameraMode(CAMERA_FREE).build();
+    raylib::RlModel model("../assets/player.iqm", "../assets/blue.png");
+    raylib::RlModelAnim anim(model.getModel(), "../assets/player.iqm");
+    std::cout << anim.getCount() << std::endl;
+    unsigned int i = 0;
+    while (window->isOpen()) {
+        raylib::DrawHelper::beginDrawing();
+        raylib::DrawHelper::clearBackground(RlColor::White);
+        raylib::DrawTextHelper::drawFps(10, 10, 30);
+        camera.update();
+        if (raylib::input::KeyboardHelper::isKeyDown(KEY_RIGHT))
+            i++;
+        if (i > anim.getCount())
+            i = 0;
+        if (raylib::input::KeyboardHelper::isKeyDown(KEY_SPACE))
+            anim.update(i);
+        if (raylib::input::KeyboardHelper::isKeyPressed(KEY_R))
+            camera.reset();
+        raylib::DrawHelper::beginMode3D(camera);
+        raylib::Shape3DHelper::drawGrid(10, 1.0f);
+        DrawModelEx(model.getModel(), {0.0f, 0.0f, 0.0f}, {0, 1, 0}, 90.0f, {0.8f, 0.8f, 0.8f}, WHITE);
+        raylib::DrawHelper::endMode3D();
+        raylib::DrawHelper::endDrawing();
+    }
     return (0);
 }
